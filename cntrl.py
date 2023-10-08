aa25195f51eb4ec3070153393b4e0a935e241078
Daichi Imai <daichi@nrobotics.com>
2023-10-08 14:34:46 +0200

    initial commit

---
+++ cntrl.py
@@ -0,0 +1,51 @@
+import numpy as np
+import time
+from lcm_msg.lcm_float32 import lcm_float32
+import lcm
+
+class cntrl:
+    def __init__(self):
+        self.send_time_stamp_from_cntrl = lcm_float32()
+        self.msg_rcv_time_stamp_from_sim = 0
+        self.loop_rate = 500
+        self.counter = 0
+
+        self.lc = lcm.LCM()
+        sub_rcv_msg_from_sim = self.lc.subscribe('/msg/send_time_stamp_from_sim', self.sub_msg_from_sim)
+
+    def sub_msg_from_sim(self, channel, data):
+        msg = lcm_float32.decode(data)
+        self.msg_rcv_time_stamp_from_sim = msg.data
+
+        cur_time = time.time_ns() / (10 ** 9)
+        cur_time -= cur_time - cur_time % 10
+        print("cntrl rcv time stamp: {:.4f}".format(self.msg_rcv_time_stamp_from_sim), " at {:.4f}".format(cur_time))
+
+    def pub_msg_from_cntrl(self):
+        self.counter += 1
+
+        self.send_time_stamp_from_cntrl.data = self.msg_rcv_time_stamp_from_sim
+        # self.send_time_stamp_from_cntrl.data = self.counter * 0.0001
+        self.lc.publish('/msg/send_time_stamp_from_cntrl', self.send_time_stamp_from_cntrl.encode())
+        cur_time = time.time_ns()/(10**9)
+        cur_time -= cur_time - cur_time % 10
+        print("cntrl send time stamp: {:.4f}".format(self.send_time_stamp_from_cntrl.data), " at {:.4f}".format( cur_time))
+
+    def main(self):
+        while True:
+            start_time = time.time_ns()
+            self.lc.handle()
+            self.pub_msg_from_cntrl()
+            mid_time = time.time_ns()
+
+            process_time = (mid_time - start_time) / (10 ** 9)
+
+            left_time = 1.0 / self.loop_rate - process_time
+            if left_time > 0:
+                time.sleep(left_time)
+            total_time = (time.time_ns() - start_time) / (10 ** 9)
+            print("cntrl: process time {:.4f}".format( process_time), "total_time {:.4f}".format(total_time))
+
+if __name__ == "__main__":
+    obj = cntrl()
+    obj.main()
